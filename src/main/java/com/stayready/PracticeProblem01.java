ec887c33d3ed94e8d514e092bfc15bb19fb30c17
Taia Williams-Rivera <tmwilliams181@students.desu.edu>
2020-07-28 13:17:03 -0400

    fixed Palindrome spelling

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,11 @@
 package com.stayready;
 
-public class PracticeProblem01 {
+public class PracticeProblem01
+{
+    public static boolean isPalidrome(String word)
+    {
+        StringBuilder builder = new StringBuilder(word);
+        String reversedWord = builder.reverse().toString();
+        return word.equals(reversedWord);
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,44 @@
 package com.stayready;
 
-public class PracticeProblem01Test {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class PracticeProblem01Test 
+{   
+
+    @Test
+    public void palindromeTest()
+    {   
+            //given
+            String word = "racecar";
+            //when
+            boolean expected = true;
+            boolean actual = PracticeProblem01.isPalidrome(word);
+            //then
+            assertEquals(expected, actual);
+    }
+    @Test
+    public void palindromeTest2()
+    {
+            //given
+            String word = "sunset";
+            //when
+            boolean expected = false;
+            boolean actual = PracticeProblem01.isPalidrome(word);
+            //then
+            assertEquals(expected, actual);
+    }
+    @Test
+    public void palindromeTest3()
+    {
+            //given
+            String word = "sos";
+            //when
+            boolean expected = true;
+            boolean actual = PracticeProblem01.isPalidrome(word);
+            //then
+            assertEquals(expected, actual);
+    }
+    
+
 }
